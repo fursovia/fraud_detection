087c82d3294f6a8a4d86af6426ca17625bc22427
fursovia <fursov.ia@gmail.com>
2021-04-09 02:30:27 +0300

    added shuffle experiments

--- bin/train.sh
+++ bin/train.sh
@@ -8,8 +8,36 @@ for config_path in ./configs/*.jsonnet; do
 
   echo ">>>>>> training ${CONFIG_NAME}"
 
-  TRAIN_DATA_PATH=./data/train.jsonl \
-    VALID_DATA_PATH=./data/valid.jsonl \
+  TRAIN_DATA_PATH=./data_full/train.jsonl \
+    VALID_DATA_PATH=./data_full/valid.jsonl \
+    allennlp train ${config_path} -s ./logs/${DATE}/${CONFIG_NAME} --include-package fraud
+
+done
+
+
+for config_path in ./configs/*.jsonnet; do
+
+  CONFIG_NAME=$(basename ${config_path})
+  CONFIG_NAME="${CONFIG_NAME%.*}"
+
+  echo ">>>>>> training ${CONFIG_NAME}"
+
+  TRAIN_DATA_PATH=./data_shuffled_train/train.jsonl \
+    VALID_DATA_PATH=./data_shuffled_train/valid.jsonl \
+    allennlp train ${config_path} -s ./logs/${DATE}/${CONFIG_NAME} --include-package fraud
+
+done
+
+
+for config_path in ./configs/*.jsonnet; do
+
+  CONFIG_NAME=$(basename ${config_path})
+  CONFIG_NAME="${CONFIG_NAME%.*}"
+
+  echo ">>>>>> training ${CONFIG_NAME}"
+
+  TRAIN_DATA_PATH=./data_shuffled_all/train.jsonl \
+    VALID_DATA_PATH=./data_shuffled_all/valid.jsonl \
     allennlp train ${config_path} -s ./logs/${DATE}/${CONFIG_NAME} --include-package fraud
 
 done
